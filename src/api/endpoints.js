32a3f7783269506122c51b2ef042c35ca1c6b330
Usman Malik <usman.malik123@redbuffer.net>
2023-07-24 16:03:56 +0500

    logs

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -2,7 +2,7 @@ const API_ENDPOINTS = {
   REGISTER: "adminAuthentication/admin/signup",
   LOGIN: "adminAuthentication/admin/signin",
   VERIFY_TOKEN: "adminRoute/admin/token-validity",
-  RUN_SCR: "scrcore/fullscr2",
+  RUN_SCR: "",
 };
 
 export default API_ENDPOINTS;
