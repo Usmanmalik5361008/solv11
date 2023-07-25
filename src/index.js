452520cca8c915d1a069aceddcb830720e464baf
Usman Malik <usman.malik123@redbuffer.net>
2023-07-25 10:39:03 +0500

    logs

--- src/index.js
+++ src/index.js
@@ -25,3 +25,23 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// In your index.js or app.js file
+
+// In your index.js or app.js file
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/firebase-messaging-sw.js", { scope: "/" })
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Error registering Service Worker:", error);
+      });
+  });
+}
